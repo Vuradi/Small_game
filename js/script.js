57a2527e661391d0e8a0866d9a327fab61d70618
Vuradisurafu <ruozversa@yandex.ru>
2022-01-10 16:52:09 +0300

    Add function random for box

--- js/script.js
+++ js/script.js
@@ -24,14 +24,22 @@ function handlerBoxClick(event) {
 function renderBox() {
     gameField.innerHTML = '';
     let box = document.createElement('div');
+    let boxSize = getRandom(30, 100);
+    let gameSize = gameField.getBoundingClientRect();
+    let maxTop = gameSize.height - boxSize;
+    let maxLeft = gameSize.width - boxSize;
 
-    box.style.height = box.style.width = '50px';
+    box.style.height = box.style.width = boxSize + 'px';
     box.style.position = 'absolute';
     box.style.backgroundColor = '#000';
-    box.style.top = '50px';
-    box.style.left = '70px';
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
     box.style.cursor = 'pointer';
     box.setAttribute('data-box', 'true');
 
     gameField.insertAdjacentElement('afterbegin', box)
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
